71e5cafb105cbf18b5a53c9d2d9ed90b85870dff
Juan Pablo Laptop Work <shunikv@gmail.com>
2024-07-17 12:10:39 -0600

    Primer commit

---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mssql',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USER_NAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities:true,
+      synchronize:true,
+      options: {
+        encrypt: false, // Disable SSL/TLS
+      },
+    })
+    
+  ],
+
+})
+export class AppModule {
+}
+  // console.log(process.env.DB_HOST);
+  // console.log(process.env.DB_PORT);
+  // console.log(process.env.DB_NAME);
+  // console.log(process.env.DB_USER_NAME);
+  // console.log(process.env.DB_PASSWORD);
